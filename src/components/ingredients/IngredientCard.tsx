bf24bcb560d4964b0ea0f8ef0096fbf7a9e34672
muruw <murumaem@gmail.com>
2023-02-26 21:21:42 +0200

    add delete and add button

--- src/components/ingredients/IngredientCard.tsx
+++ src/components/ingredients/IngredientCard.tsx
@@ -2,15 +2,38 @@ import { Ingredient } from "./ingredient";
 
 interface ComponentProps {
   ingredient: Ingredient;
+  onDelete: (i: Ingredient) => void;
 }
 
-export const IngredientCard = ({ ingredient }: ComponentProps) => {
+export const IngredientCard = ({ ingredient, onDelete }: ComponentProps) => {
+  const handleDelete = () => {
+    onDelete(ingredient);
+  };
+
   return (
-    <div className="p-4">
-      <a
-        href="#"
-        className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+    <div className="bg-white border border-gray-200 rounded-lg shadow dark:border-gray-700 dark:bg-gray-800 ">
+      <button
+        type="button"
+        className="flex justify-end px-4 pt-4 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto dark:hover:bg-gray-600 dark:hover:text-white"
+        data-modal-hide="defaultModal"
+        onClick={handleDelete}
       >
+        <svg
+          aria-hidden="true"
+          className="w-5 h-5"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+        <span className="sr-only">Delete item</span>
+      </button>{" "}
+      <div className="flex flex-col items-center md:flex-row md:max-w-md">
         <img
           className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
           src={ingredient.imageUrl}
@@ -28,7 +51,7 @@ export const IngredientCard = ({ ingredient }: ComponentProps) => {
             {ingredient.amount} / {ingredient.measurementUnit}
           </p>
         </div>
-      </a>
+      </div>
     </div>
   );
 };
--- src/components/ingredients/IngredientView.tsx
+++ src/components/ingredients/IngredientView.tsx
@@ -1,7 +1,8 @@
+import { useState } from "react";
 import { Ingredient } from "./ingredient";
 import { IngredientCard } from "./IngredientCard";
 
-const ingredients: Ingredient[] = [
+const defaultIngredients: Ingredient[] = [
   {
     name: "Triple sec",
     imageUrl:
@@ -26,11 +27,39 @@ const ingredients: Ingredient[] = [
 ];
 
 export const IngredientView = () => {
+  const [ingredients, setIngredients] =
+    useState<Ingredient[]>(defaultIngredients);
+
+  const onAddIngredient = () => {
+    const newIngrediate = {
+      name: "Beefeater Gin",
+      imageUrl:
+        "https://www.beefeatergin.com/wp-content/uploads/2023/01/Beefeater-London-Dry-Gin-aspect-ratio-320-540.png",
+      amount: 700,
+      measurementUnit: "ml",
+    };
+    setIngredients([...ingredients, newIngrediate]);
+  };
+
+  const onDelete = (itemToBeDeleted: Ingredient) => {
+    let filteredArray = ingredients.filter((item) => item !== itemToBeDeleted);
+    setIngredients(filteredArray);
+  };
+
   return (
-    <div className="flex flex-wrap">
-      {ingredients.map((i: Ingredient, key: number) => (
-        <IngredientCard ingredient={i} key={key} />
-      ))}
+    <div className="flex flex-col items-center">
+      <button
+        type="button"
+        onClick={onAddIngredient}
+        className="max-w-xs text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+      >
+        Add
+      </button>
+      <div className="flex flex-wrap gap-4">
+        {ingredients.map((i: Ingredient, key: number) => (
+          <IngredientCard ingredient={i} key={key} onDelete={onDelete} />
+        ))}
+      </div>
     </div>
   );
 };
