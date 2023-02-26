0169ef00249f480f9d0a3326fcb5eb367acc10c4
muruw <murumaem@gmail.com>
2023-02-26 14:34:25 +0200

    Add inventory view

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import './index.css';
-import App from './App';
-import { HelloWorld } from './HelloWorld';
-import Root from './routes/Root';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import { HelloWorld } from "./HelloWorld";
+import Root from "./routes/Root";
+import { IngredientView } from "./components/ingredients/IngredientView";
 
 const router = createBrowserRouter([
   {
@@ -16,15 +13,23 @@ const router = createBrowserRouter([
     element: <Root />,
     children: [
       {
+        path: "/",
+        element: <IngredientView />,
+      },
+      {
+        path: "/details/:ingredient",
+        element: <IngredientView />,
+      },
+      {
         path: "/recipes",
-        element: <HelloWorld name="Siim"/>
-      }
+        element: <HelloWorld name="Siim" />,
+      },
     ],
   },
 ]);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
